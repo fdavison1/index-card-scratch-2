b6c1c04ad869ad1773158523bac03417141b6e31
Fred Davison <fredbook@Freds-MacBook-Pro.local>
2019-10-31 19:59:10 -0600

    component skeleton

---
+++ src/components/Display1.jsx
@@ -0,0 +1,20 @@
+import React from 'react'
+import CardTemplate from './CardTemplate'
+
+export default class Display1 extends React.Component {
+    constructor() {
+        super()
+        this.state = {
+
+        }
+    }
+    render() {
+        return (
+            <div>
+                Display1.jsx
+                <CardTemplate />
+
+            </div>
+        )
+    }
+}
